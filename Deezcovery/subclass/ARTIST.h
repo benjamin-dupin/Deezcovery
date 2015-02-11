850af5fdcf1bb4bdf6fc71f3a6e57759196d0b52
Lorpoce <lorpoce@gmail.com>
2015-02-11 17:59:21 +0100

    Db

---
+++ Deezcovery/subclass/ARTIST.h
@@ -0,0 +1,17 @@
+//
+//  ARTIST.h
+//  Deezcovery
+//
+//  Created by B'n'J on 11/02/2015.
+//  Copyright (c) 2015 B'n'J. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface ARTIST : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * id;
+
+@end
---
+++ Deezcovery/subclass/ARTIST.m
@@ -0,0 +1,16 @@
+//
+//  ARTIST.m
+//  Deezcovery
+//
+//  Created by B'n'J on 11/02/2015.
+//  Copyright (c) 2015 B'n'J. All rights reserved.
+//
+
+#import "ARTIST.h"
+
+
+@implementation ARTIST
+
+@dynamic id;
+
+@end
